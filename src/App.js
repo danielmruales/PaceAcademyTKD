41dd0c187a551274d3d25b1edbb17b3344933a85
Daniel Carrera <danielmruales@gmail.com>
2020-06-23 01:36:18 -0600

    added navbar folder and used code from my personal portfolio to setup a template

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Navbar from './Navbar/Navbar.js'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Navbar/>
+      <Switch>
+          <Route>
+
+
+          </Route>
+
+
+
+      </Switch>
       Sup
     </div>
   );
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+// import logo from '../DMR.png'
+import './Navbar.css'
+
+const Nabar = (props) => {
+    return (
+        <div className='nav'>
+            
+            {/* <Link to='/'><img src={logo} alt='logo' className='logo'/></Link> */}
+            <Link to='/projects' className='navItems'>Projects</Link>
+            <Link to='/resume' className='navItems'>Resume</Link>
+            <Link to='/about' className='navItems'>About</Link>
+            {/* <Link to='/recommendations' className='navItems'>Recommendations</Link> */}
+            <Link to='/contact' className='navItems'>Contact</Link>
+        </div>
+    );
+};
+
+export default Nabar;
