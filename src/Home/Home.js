b1436ae6b176d1e5717d49640dae79fbf2d7d69a
Daniel Carrera <danielmruales@gmail.com>
2020-07-02 01:39:26 -0600

    added sections to home page. Began work on section 2. Added Logo to navbar and tab favicon

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -5,11 +5,18 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <div className='homePageMain'>
-                    
+                <div className='heroImage'>
+                   What is up? 
                 </div>
-
-                <div className='secondHomePage'>
+                <div className='section1'>
+                    <h1 className='sec1Title1'>
+                        Creating <span style={{color:'#E5B02C'}}>Opportunity</span>  in all Areas of Life
+                    </h1>
+                </div>
+                <div className='section2'>
+                    Hello
+                </div>
+                <div className='section3'>
                     Hello
                 </div>
             </div>
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
-// import logo from '../DMR.png'
+import logo from '../PaceLogo.png'
 import './Navbar.css'
 
 const Nabar = (props) => {
     return (
         <div className='nav'>
             
-            {/* <Link to='/'><img src={logo} alt='logo' className='logo'/></Link> */}
-            <Link to='/' className='navItems'>LogoGoesHere</Link>
+            <Link to='/'><img src={logo} alt='logo' className='logo'/></Link>
+            {/* <Link to='/' className='navItems'>LogoGoesHere</Link> */}
             <Link to='/projects' className='navItems'>Projects</Link>
             <Link to='/resume' className='navItems'>Resume</Link>
             <Link to='/about' className='navItems'>About</Link>
